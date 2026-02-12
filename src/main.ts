6b82234c8e209e74b84772a06dd8c87521d7b76c
lemonrick <>
2026-02-12 23:15:41 +0100

    Revert number format to English

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,10 @@
-import {registerLocaleData} from "@angular/common";
-import localeSk from "@angular/common/locales/sk";
-import { LOCALE_ID, provideZoneChangeDetection } from "@angular/core";
+import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 
-registerLocaleData(localeSk);
-
 bootstrapApplication(AppComponent, {
   providers: [
-    provideZoneChangeDetection(),
-    { provide: LOCALE_ID, useValue: 'sk-SK' }
+    provideZoneChangeDetection()
   ]
 }).catch((err) => console.error(err));
