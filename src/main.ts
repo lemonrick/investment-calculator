c068cfbb5ee8b5ed8f973f60b04910f82fecef09
lemonrick <>
2026-02-12 23:11:45 +0100

    Use Slovak number formatting

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
-import { provideZoneChangeDetection } from "@angular/core";
+import {registerLocaleData} from "@angular/common";
+import localeSk from "@angular/common/locales/sk";
+import { LOCALE_ID, provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, {providers: [provideZoneChangeDetection()]}).catch((err) => console.error(err));
+registerLocaleData(localeSk);
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideZoneChangeDetection(),
+    { provide: LOCALE_ID, useValue: 'sk-SK' }
+  ]
+}).catch((err) => console.error(err));
